2a195b030cebcf4039458cbb56c3fd0e50fdca9b
maxim092001 <35135337+maxim092001@users.noreply.github.com>
2019-12-17 16:13:43 +0300

    ReverseMin tests added

---
+++ prog-intro-homework/reverseMin/FastReverseMinTest.java
@@ -0,0 +1,10 @@
+package reverse;
+
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public class FastReverseMinTest {
+    public static void main(String... args) {
+        new ReverseMinTest(FastReverseTest.MAX_SIZE).run();
+    }
+}
