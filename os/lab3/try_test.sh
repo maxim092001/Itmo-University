3340012f7aeb3c065230e9316ccd58fab147f593
root <root@Ubuntu.ocnajrefcouuvjd15ttmrj3g1e.bx.internal.cloudapp.net>
2020-10-28 10:12:15 +0000

    lab3

--- os/lab3/try_test.sh
+++ os/lab3/try_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curDate=$(date '+%d.%m.%y_%H:%M:%S')
 (mkdir ~/lab3/test && echo "catalog test was created successfully" >> ~/lab3/report && touch ~/lab3/test/$curDate)
-(ping -c 1 http://www.net_nikogo.ru/) || (echo "No answer from net_nikogo.ru lol" >> ~/lab3/report)
+(ping -c 1 http://www.net_nikogo.ru/) || (echo "$curDate No answer from net_nikogo.ru lol" >> ~/lab3/report)
 
