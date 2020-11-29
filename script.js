ae633b181d9090dce5a19e448c75c3474b7f6fd9
Maxim <maximgran@gmail.com>
2020-11-29 17:03:12 +0300

    script.js added

---
+++ script.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+    const ctx = document.getElementById("myChart");
+    const data = {
+        labels: [1, 2, 3, 4, 5],
+        datasets: [{
+            label: "f(x) = x",
+            function: function (x) {
+                return x
+            },
+            borderColor: "rgba(75, 192, 192, 1)",
+            data: [],
+            fill: false
+        },
+            {
+                label: "f(x) = x?",
+                function: function (x) {
+                    return x * x
+                },
+                borderColor: "rgba(153, 102, 255, 1)",
+                data: [],
+                fill: false
+            },
+            {
+                label: "f(x) = x * log(x)",
+                function: function (x) {
+                    return x * Math.log(x)
+                },
+                borderColor: "rgba(255, 206, 86, 1)",
+                data: [],
+                fill: false
+            }]
+    };
+    Chart.plugins.register({
+        beforeInit: function (chart) {
+            const data = chart.config.data;
+            for (let i = 0; i < data.datasets.length; i++) {
+                for (let j = 0; j < data.labels.length; j++) {
+                    const fct = data.datasets[i].function,
+                        x = data.labels[j],
+                        y = fct(x);
+                    data.datasets[i].data.push(y);
+                }
+            }
+        }
+    });
+    const myBarChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
