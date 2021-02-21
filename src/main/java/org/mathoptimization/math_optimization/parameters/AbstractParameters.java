23d60728a0ad4ec5d5cf9047dac0337c5ddc9b47
Maxim Grankin <maximgran@gmail.com>
2021-02-21 21:57:32 +0300

    made AbstractParameters abstract (lol)

--- src/main/java/org/mathoptimization/math_optimization/parameters/AbstractParameters.java
+++ src/main/java/org/mathoptimization/math_optimization/parameters/AbstractParameters.java
@@ -5,14 +5,11 @@ import lombok.ToString;
 
 @ToString
 @Data
-public class AbstractParameters implements Parameters {
+public abstract class AbstractParameters implements Parameters {
     protected Double left;
     protected Double right;
     protected Double value;
     protected Double argument;
-
-    public AbstractParameters() {
-    }
 
     public AbstractParameters(final Double left, final Double right, final Double value, final Double argument) {
         this.left = left;
