63505bbb4e4baa2187c04ff6b0ce8b0b3eadaf95
mg@cone.ee <maximgran092001>
2021-02-21 17:11:57 +0300

    initial

---
+++ src/main/java/org/mathoptimization/math_optimization/MathOptimizationApplication.java
@@ -0,0 +1,13 @@
+package org.mathoptimization.math_optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathOptimizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mathoptimization/math_optimization/MathOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mathoptimization.math_optimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
