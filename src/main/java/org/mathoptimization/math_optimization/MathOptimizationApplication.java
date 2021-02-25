d902f74e1641085d58dddd74ad76e3bab72d4240
Georgiy Nazarov <geranazavr555@yandex.ru>
2021-02-26 01:21:37 +0300

    #f(x)(eps) counters

--- src/main/java/org/mathoptimization/math_optimization/MathOptimizationApplication.java
+++ src/main/java/org/mathoptimization/math_optimization/MathOptimizationApplication.java
@@ -5,11 +5,125 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+import java.text.DecimalFormat;
+import java.util.function.Function;
+
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MathOptimizationApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(MathOptimizationApplication.class, args);
+//		 SpringApplication.run(MathOptimizationApplication.class, args);
+
+//		Function<Double, OptimizationMethod> dichotomyMethodFactory = (eps) -> new DichotomyMethod(
+//				6.0,
+//				9.9,
+//				x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
+//				eps,
+//				1e-10
+//		);
+//
+//		double[] epss = {1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7, 5e-8, 1e-8, 5e-9, 1e-9};
+//		for (double eps : epss) {
+//			OptimizationMethod method = dichotomyMethodFactory.apply(eps);
+//			method.calculate();
+//
+//			System.out.printf("%.10f", eps);
+//			System.out.print(", ");
+//			System.out.print(method.getParameters().size() * 2);
+//			System.out.print(", ");
+//			System.out.print(method.getMinimumArgument());
+//			System.out.print(", ");
+//			System.out.println(method.getMinimumValue());
+//		}
+
+//
+//		Function<Double, OptimizationMethod> fibonacciMethodFactory = (eps) -> new FibonacciMethod(
+//				6.0,
+//				9.9,
+//				x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
+//				eps
+//		);
+//
+//		double[] epss = {1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7, 5e-8, 1e-8, 5e-9, 1e-9};
+//		for (double eps : epss) {
+//			OptimizationMethod method = fibonacciMethodFactory.apply(eps);
+//			method.calculate();
+//
+//			System.out.printf("%.10f", eps);
+//			System.out.print(", ");
+//			System.out.print(method.getParameters().size() * 2 + 1);
+//			System.out.print(", ");
+//			System.out.print(method.getMinimumArgument());
+//			System.out.print(", ");
+//			System.out.println(method.getMinimumValue());
+//		}
+//
+//		Function<Double, OptimizationMethod> parabolaMethodFactory = (eps) -> new FibonacciMethod(
+//				6.0,
+//				9.9,
+//				x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
+//				eps
+//		);
+//
+//		double[] epss = {1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7, 5e-8, 1e-8, 5e-9, 1e-9};
+//		for (double eps : epss) {
+//			OptimizationMethod method = parabolaMethodFactory.apply(eps);
+//			method.calculate();
+//
+//			System.out.printf("%.10f", eps);
+//			System.out.print(", ");
+//			System.out.print(method.getParameters().size() * 3 + 1);
+//			System.out.print(", ");
+//			System.out.print(method.getMinimumArgument());
+//			System.out.print(", ");
+//			System.out.println(method.getMinimumValue());
+//		}
+
+
+//		Function<Double, OptimizationMethod> goldenRatio = (eps) -> new GoldenRatioMethod(
+//				6.0,
+//				9.9,
+//				x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
+//				eps
+//		);
+//
+//		double[] epss = {1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7, 5e-8, 1e-8, 5e-9, 1e-9};
+//		for (double eps : epss) {
+//			OptimizationMethod method = goldenRatio.apply(eps);
+//			method.calculate();
+//
+//			System.out.printf("%.10f", eps);
+//			System.out.print(", ");
+//			System.out.print(method.getParameters().size() * 2 + 1);
+//			System.out.print(", ");
+//			System.out.print(method.getMinimumArgument());
+//			System.out.print(", ");
+//			System.out.println(method.getMinimumValue());
+//		}
+
+
+		Function<Double, OptimizationMethod> brents = (eps) -> new BrentsMethod(
+				6.0,
+				9.9,
+				x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
+				eps
+		);
+
+		double[] epss = {1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7, 5e-8, 1e-8, 5e-9, 1e-9};
+		for (double eps : epss) {
+			OptimizationMethod method = brents.apply(eps);
+			method.calculate();
+
+			System.out.printf("%.10f", eps);
+			System.out.print(", ");
+			System.out.print(method.getParameters().size() + 2);
+			System.out.print(", ");
+			System.out.print(method.getMinimumArgument());
+			System.out.print(", ");
+			System.out.println(method.getMinimumValue());
+		}
+
+
 
 //		OptimizationMethod ratioMethod = new GoldenRatioMethod(
 //				6.0,
