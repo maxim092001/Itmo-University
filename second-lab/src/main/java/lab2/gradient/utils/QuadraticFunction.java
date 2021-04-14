c93256ed98a30628108ab1bc602d6d85fe25a14c
Maxim Grankin <maximgran@gmail.com>
2021-04-14 21:22:55 +0300

    docs for quadratic function.

--- second-lab/src/main/java/lab2/gradient/utils/QuadraticFunction.java
+++ second-lab/src/main/java/lab2/gradient/utils/QuadraticFunction.java
@@ -24,9 +24,10 @@ public class QuadraticFunction implements Function<Vector, Double> {
 
     /**
      * Quadratic function constructor.
-     * @param a
-     * @param b
-     * @param c
+     *
+     * @param a matrix.
+     * @param b vector
+     * @param c coefficient.
      */
     public QuadraticFunction(final Matrix a, final Vector b, final double c) {
         int n = a.verticalLength();
@@ -48,35 +49,84 @@ public class QuadraticFunction implements Function<Vector, Double> {
         this.c = c;
     }
 
+    /**
+     * Get function result in point.
+     *
+     * @param point vector representation of point
+     * @return function result.
+     */
     @Override
-    public Double apply(final Vector v) {
-        return 0.5 * ((Vector) a.mul(v)).scalarMul(v) + b.scalarMul(v) + c;
+    public Double apply(final Vector point) {
+        return 0.5 * ((Vector) a.mul(point)).scalarMul(point) + b.scalarMul(point) + c;
     }
 
+    /**
+     * Getter for a.
+     *
+     * @return a.
+     */
     public Matrix getA() {
         return a;
     }
 
+    /**
+     * Getter for b.
+     *
+     * @return b.
+     */
     public Vector getB() {
         return b;
     }
 
+    /**
+     * Getter for c.
+     *
+     * @return c.
+     */
     public double getC() {
         return c;
     }
 
-    public Vector gradient(final Vector x) {
-        return ((Vector) getA().mul(x)).add(getB());
+    /**
+     * Find gradient for function in point.
+     *
+     * @param point given point
+     * @return gradient.
+     */
+    public Vector gradient(final Vector point) {
+        return ((Vector) getA().mul(point)).add(getB());
     }
 
-    public Vector antiGradient(Vector x) {
-        return gradient(x).mul(-1);
+    /**
+     * Find anti gradient for function in point
+     *
+     * @param point given point.
+     * @return anti gradient.
+     */
+    public Vector antiGradient(final Vector point) {
+        return gradient(point).mul(-1);
     }
 
+    /**
+     * Dimensions.
+     *
+     * @return dimensions.
+     */
     public int dimensions() {
         return a.verticalLength();
     }
 
+    /**
+     * Create quadratic function from 2d. {@code Ax^2 + Bxy + C y^2 + Dx + Ey + F}
+     *
+     * @param a coefficient.
+     * @param b coefficient.
+     * @param c coefficient.
+     * @param d coefficient.
+     * @param e coefficient.
+     * @param f coefficient.
+     * @return created quadratic function.
+     */
     // A x^2 + B xy + C y^2 + Dx + Ey + F
     public static QuadraticFunction from2d(double a, double b, double c, double d, double e, double f) {
         double[][] A = new double[][]{
