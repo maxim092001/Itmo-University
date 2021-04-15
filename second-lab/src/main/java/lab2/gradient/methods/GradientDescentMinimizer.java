ee8db2762cd8750fb91275d5dac7031098f4cef3
VankaTaganai <panovivan01@yandex.ru>
2021-04-15 22:54:39 +0300

    hui

--- second-lab/src/main/java/lab2/gradient/methods/GradientDescentMinimizer.java
+++ second-lab/src/main/java/lab2/gradient/methods/GradientDescentMinimizer.java
@@ -30,19 +30,46 @@ public class GradientDescentMinimizer extends AbstractGradientMinimizer {
         }
     }
 
+    public MinimizationResult hui(
+            final QuadraticFunction f,
+            final Vector point,
+            final double eps,
+            double alpha
+    ) {
+        int numberOfIterations = 0;
+        List<IterationStep> steps = new ArrayList<>();
+        Vector gradient = f.gradient(point);
+        Vector xk = point;
+        Double fX = f.apply(xk);
+        while(gradient.rate() > eps) {
+            steps.add(new IterationStep(numberOfIterations, xk, fX));
+            Vector y = xk.sub(gradient.mul(alpha));
+            Double fY = f.apply(y);
+            while (fY > fX) {
+                alpha /= 2;
+                y = xk.sub(gradient.mul(alpha));
+                fY = f.apply(y);
+            }
+            xk = y;
+            fX = fY;
+            gradient = f.gradient(xk);
+            numberOfIterations++;
+        }
+        steps.add(new IterationStep(numberOfIterations, xk, fX));
+        return MinimizationResult.of(xk, fX, numberOfIterations, steps);
+    }
+
     public static void main(String[] args) {
         // lambda1 = 4 - 2 sqrt 2, lambda2 = 4 + 2 sqrt 2
-        QuadraticFunction f1 = QuadraticFunction.from2d(1, 2, 3, 4, 5, 6);
+        QuadraticFunction f1 = QuadraticFunction.from2d(64, 126, 64, -10, 30, 13);
         System.out.println(f1);
-        Vector startPoint = new Vector(0.0, 0.0);
+        Vector startPoint = new Vector(10.0, 15.0);
         List<IterationStep> steps = new ArrayList<>();
-        MinimizationResult result = new GradientDescentMinimizer().iteration(
+        MinimizationResult result = new GradientDescentMinimizer().hui(
                 f1,
-                0.25,
-                1e-5,
                 startPoint,
-                0L,
-                steps
+                1e-5,
+                2 / 256.
         );
         System.out.println(result);
 
