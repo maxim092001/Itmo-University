d98407c83cc189c4b61ceb120a966397ae5f68b1
Maxim Grankin <maximgran@gmail.com>
2021-04-16 23:26:33 +0300

    javadoc IterationStep

--- second-lab/src/main/java/lab2/gradient/methods/IterationStep.java
+++ second-lab/src/main/java/lab2/gradient/methods/IterationStep.java
@@ -2,15 +2,57 @@ package lab2.gradient.methods;
 
 import lab2.gradient.utils.Vector;
 
+/**
+ * Iteration step POJO.
+ */
 public class IterationStep {
+
+    /**
+     * Step number.
+     */
     private final long stepNumber;
+
+    /**
+     * Vector (point).
+     */
     private final Vector vector;
+
+    /**
+     * Function result in {@link IterationStep#vector}
+     */
     private final double functionResult;
 
     public IterationStep(final long stepNumber, final Vector vector, final double functionResult) {
         this.stepNumber = stepNumber;
         this.vector = vector;
         this.functionResult = functionResult;
+    }
+
+    /**
+     * Get step number.
+     *
+     * @return step number.
+     */
+    public long getStepNumber() {
+        return stepNumber;
+    }
+
+    /**
+     * Get vector.
+     *
+     * @return vector.
+     */
+    public Vector getVector() {
+        return vector;
+    }
+
+    /**
+     * Get function result in {@link IterationStep#vector}.
+     *
+     * @return function result.
+     */
+    public double getFunctionResult() {
+        return functionResult;
     }
 
     @Override
