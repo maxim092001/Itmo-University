c3cb525f2e0f19fd7803914d59b2b7a1e3509f8c
VankaTaganai <panovivan01@yandex.ru>
2021-06-02 23:26:20 +0300

    Powell all

---
+++ fourth-lab/src/main/java/lab4/newton/PowellMethod.java
@@ -0,0 +1,47 @@
+package lab4.newton;
+
+import lab4.matrix.FullMatrix;
+import lab4.matrix.Vector;
+import lab4.methods.GoldenRatioMethod;
+
+import java.util.function.Function;
+
+public class PowellMethod extends AbstractNewtonMethod {
+    public PowellMethod(final Function<Vector, Double> function, final Double eps, final Vector startPoint) {
+        super(function, eps, startPoint);
+    }
+
+    @Override
+    public double getAlpha(final Vector xk, final Vector pk) {
+        var method = new GoldenRatioMethod(-1000d, 1000d, x -> function.apply(xk.add(pk.mul(x))), eps);
+        method.calculate();
+        return method.getMinArgument();
+    }
+
+    @Override
+    public Vector minimize() {
+        FullMatrix prevG = FullMatrix.identityMatrix(size);
+        Vector prevW = gradient(startPoint).mul(-1.0);
+        Vector prevP = prevW;
+        double ak = getAlpha(startPoint, prevP);
+        Vector prevX = startPoint.add(prevP.mul(ak));
+        Vector prevDX = prevX.sub(startPoint);
+
+        while (prevDX.norm() > eps) {
+            Vector nextW = gradient(prevX).mul(-1.0);
+            Vector dw = nextW.sub(prevW);
+            Vector v = prevG.multiply(dw);
+            Vector dWaveX = prevDX.add(prevG.multiply(dw));
+            FullMatrix nextG = prevG.sub(dWaveX.mulByTransposed(dWaveX).div(dw.scalarMultiply(dWaveX)));
+            Vector nextP = nextG.multiply(nextW);
+            ak = getAlpha(prevX, nextP);
+            Vector nextX = prevX.add(nextP.mul(ak));
+
+            prevG = nextG;
+            prevW = nextW;
+            prevX = nextX;
+            prevDX = nextX.sub(prevX);
+        }
+        return prevX;
+    }
+}
